dc7ccb34c047475be649a28c8176bf7826a99d83
dragoonflyPJ <dragoonflypj@gmail.com>
2024-12-31 02:04:47 -0500

    Update testIconScr.gd

--- githubtest/TestNodes/testIconScr.gd
+++ githubtest/TestNodes/testIconScr.gd
@@ -3,5 +3,5 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hola, prueba con GDScript")
+	print("Testeo de Cambio con GDScript")
 	pass # Replace with function body.
