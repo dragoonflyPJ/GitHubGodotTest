e7b10297fc66dfa13eaca9e3456d2342442fc62b
dragoonflyPJ <dragoonflypj@gmail.com>
2024-12-31 01:09:09 -0500

    StartProject
    Link Godot Project and GitHub

---
+++ githubtest/TestNodes/testCSharpScr.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class testCSharpScr : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready(){
+		GD.Print("Prueba con un Script de c#");
+	}
+
+}
