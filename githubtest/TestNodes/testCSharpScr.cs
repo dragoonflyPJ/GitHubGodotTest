70346dc4220e657cd06695fd0710e175dd9ff2da
dragoonflyPJ <dragoonflypj@gmail.com>
2024-12-31 01:57:13 -0500

    Update testCSharpScr.cs

--- githubtest/TestNodes/testCSharpScr.cs
+++ githubtest/TestNodes/testCSharpScr.cs
@@ -5,7 +5,7 @@ public partial class testCSharpScr : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
-		GD.Print("Prueba con un Script de c#");
+		GD.Print("Esto es una Prueba editando desde el Github");
 	}
 
 }
