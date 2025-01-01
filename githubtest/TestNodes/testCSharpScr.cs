cb76d9b7d3e7b0b20b683185baa32e4820b4d3d4
Mr FloodGate <isaacmorafloodgate@gmail.com>
2025-01-01 13:21:05 -0800

    Update testCSharpScr.cs by Mr.FloodGate
    jaja jaja jaja

--- githubtest/TestNodes/testCSharpScr.cs
+++ githubtest/TestNodes/testCSharpScr.cs
@@ -5,7 +5,17 @@ public partial class testCSharpScr : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
+ 	Godot.Array<int> primes = new();
 		GD.Print("Prueba de Edicion desde GitHub, nueva cuenta 123");
+  		for(int i = 0; i < 100; i++){
+    			bool prime = false;
+    			for(int ni = 0; ni < primes.Size(); ni++) {
+      				if((float) i % ni == 0) {
+	  				prime = true; 
+      					 break;
+      				}
+	  		if(prime) primes.Add(i);
+    		}
 	}
 
 }
