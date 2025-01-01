6c5ecb4d1cbe51b5665bd5431438bee65b9e8205
dragoonflyPJ <dragoonflypj@gmail.com>
2025-01-01 16:02:24 -0500

    Cambio de letra a 123

--- githubtest/TestNodes/testCSharpScr.cs
+++ githubtest/TestNodes/testCSharpScr.cs
@@ -5,7 +5,7 @@ public partial class testCSharpScr : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
-		GD.Print("Esto es una Prueba editando desde el Github");
+		GD.Print("Prueba de Edicion desde GitHub, nueva cuenta 123");
 	}
 
 }
