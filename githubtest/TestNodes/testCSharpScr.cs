ba8e11028185131ce924d57bc5e4e45a8e83c0f6
dragoonflyPJ <dragoonflypj@gmail.com>
2025-01-01 16:33:20 -0500

    Update testCSharpScr.cs

--- githubtest/TestNodes/testCSharpScr.cs
+++ githubtest/TestNodes/testCSharpScr.cs
@@ -5,20 +5,7 @@ public partial class testCSharpScr : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
- 	Godot.Array<int> primes = new();
 		GD.Print("Prueba de Edicion desde GitHub, nueva cuenta 123");
-  		for(int i = 0; i < 100; i++){
-    			bool prime = true;
-    			for(int ni = 0; ni < primes.Size(); ni++) {
-      				if((float) i % ni == 0) {
-	  				prime = false; 
-      					 break;
-      				}
-	  		if(prime) {
-     				primes.Add(i);
-	 			GD.Print(i);
-    			}
-    		}
 	}
 
 }
